aaaa38359e7140cc6c44a1ec75fd3ce7e3717cef
Tanay Yaduka <156800402+TanayYaduka@users.noreply.github.com>
2025-06-16 22:58:18 +0530

    Update app.py

--- Deployment/app.py
+++ Deployment/app.py
@@ -6,78 +6,66 @@ import urllib.request
 import gdown
 import os
 
-from utils import gen_labels, preprocess, model_arc  # make sure utils.py contains these functions
+from utils import gen_labels, preprocess, model_arc
 
-# Generate labels
+# Get labels
 labels = gen_labels()
 
-# HTML and UI Design
-html_temp = '''
+# App header
+st.markdown('''
   <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; margin-top: -50px">
     <div style="display: flex; flex-direction: row; align-items: center; justify-content: center;">
       <center><h1 style="color: #000; font-size: 50px;"><span style="color: #0e7d73">Smart </span>Garbage</h1></center>
-      <img src="https://cdn-icons-png.flaticon.com/128/1345/1345823.png" style="width: 0px;">
     </div>
     <div style="margin-top: -20px">
       <img src="https://i.postimg.cc/W3Lx45QB/Waste-management-pana.png" style="width: 400px;">
-    </div>  
-  </div>
-'''
-st.markdown(html_temp, unsafe_allow_html=True)
-
-st.markdown('''
-  <div>
-    <center><h3 style="color: #008080; margin-top: -20px">Check the type here </h3></center>
+    </div>
   </div>
 ''', unsafe_allow_html=True)
 
+st.markdown('''
+  <div><center><h3 style="color: #008080; margin-top: -20px">Check the type here</h3></center></div>
+''', unsafe_allow_html=True)
 
-
-# Upload Option
-opt = st.selectbox("How do you want to upload the image for classification?", 
+opt = st.selectbox("How do you want to upload the image for classification?",
                    ('Please Select', 'Upload image via link', 'Upload image from device'))
 
 image = None
 
+# Upload options
 if opt == 'Upload image from device':
     file = st.file_uploader('Select an image', type=['jpg', 'png', 'jpeg'])
     if file is not None:
         image = Image.open(file)
 
 elif opt == 'Upload image via link':
-    try:
-        img_url = st.text_input('Enter the Image Address')
-        if img_url:
+    img_url = st.text_input('Enter the Image URL')
+    if img_url:
+        try:
             image = Image.open(urllib.request.urlopen(img_url))
-    except:
-        if st.button('Submit'):
-            show = st.error("Please Enter a valid Image Address!")
-            time.sleep(4)
-            show.empty()
+        except:
+            st.error("Invalid Image URL")
 
-# If image is uploaded, show preview
+# Display and predict
 if image is not None:
     st.image(image, width=300, caption='Uploaded Image')
 
     if st.button('Predict'):
         st.info("Processing...")
 
-        # Preprocess image
         img = preprocess(image)
 
-        # Download model from Google Drive if not already present
+        # Download model from Google Drive if not already downloaded
         file_id = '19UR26IpbztBfWM_uVMlE6rv240sssr9D'
         output_path = 'modelnew.h5'
         if not os.path.exists(output_path):
             gdown.download(f'https://drive.google.com/uc?id={file_id}', output_path, quiet=False)
 
-        # Load model
+        # Load model and predict
         model = model_arc()
         model.load_weights(output_path)
 
-        # Make prediction
         prediction = model.predict(img[np.newaxis, ...])
         result = labels[np.argmax(prediction[0], axis=-1)]
 
-        st.success(f'Hey! The uploaded image has been classified as **"{result} product"**.')
-
+        st.success(f'The uploaded image has been classified as **"{result}"**.')
